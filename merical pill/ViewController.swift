9ecefc077f63f1f95715a358d86da596678d072a
Terno720 <lwlridesnow@gmail.com>
2017-06-29 16:05:59 -0600

    commit 1

--- merical pill/ViewController.swift
+++ merical pill/ViewController.swift
@@ -8,11 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var postalCode: UILabel!
+    @IBOutlet weak var postalCodeTextFeild: UITextField!
+   
+    @IBOutlet weak var provincePicker: UIPickerView!
+    
+    @IBOutlet weak var provinceButton: UIButton!
+    
+    let province = ["AB", "BC", "wetaskiwin"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        provincePicker.dataSource = self
+        provincePicker.delegate = self
+    
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +33,34 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func provinceButtonPressed(_ sender: Any) {
+        
+        provincePicker.isHidden = false
+        postalCode.isHidden = true
+        postalCodeTextFeild.isHidden = true
+        
+    }
+    
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return province.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return province[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        provinceButton.setTitle(province[row], for: UIControlState.normal)
+        provincePicker.isHidden = true
+        postalCode.isHidden = false 
+        postalCodeTextFeild.isHidden = false
+    }
 
 }
 
